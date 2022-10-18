d778dde2fd13cd8266bf071412257b4457ee5487
n13iq2 <sitvili@gmail.com>
2022-10-18 11:25:26 +0200

    j

--- elso.py
+++ elso.py
@@ -1,4 +1,5 @@
 from hatodik2 import Option
+from matplotlib import pyplot as plt
 #opt=Option("C",100,"20221215",1)
 #print(opt.calcPayoff(139))
 K=100
@@ -10,3 +11,11 @@ t=0.23
 vola=0.45
 
 print(C.calcPrice(S,t,vola)+P.calcPrice(S,t,vola)-S)
+
+spots=range(250,500,5)
+prices=[C.calcPrice(s,1,vola) for s in spots]
+pays=[C.calcPayoff(s) for s in spots]
+
+plt.plot(spots,pays,spots,prices)
+plt.show()
+
