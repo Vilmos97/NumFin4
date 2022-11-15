66e9c67960a333394cf30fd959201c1f515306aa
n13iq2 <sitvili@gmail.com>
2022-11-15 08:28:21 +0100

    j

--- 9het.py
+++ 9het.py
@@ -6,7 +6,13 @@ df = pd.read_csv(filename)
 df['ret_log'] = np.log(df['Adj Close']/df['Adj Close'].shift(1))
 
 df.index = pd.to_datetime(df['Date'])
-df['ret_log'].plot()
+t_day_in_year = 252
 
+df['vol_rolling_1y']=df['ret_log'].rolling(252).std()
+df['vol_rolling_2y']=df['ret_log'].rolling(504).std()
+
+df[['vol_rolling_1y', 'vol_rolling_2y']].plot()
+
+#df['ret_log'].plot()
 plt.show()
 print(1)
